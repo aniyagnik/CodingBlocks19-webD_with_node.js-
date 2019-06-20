72cb9b9e456ee986748e73aebe56a36869f3aef2
anirudh yagnik <ani789yagnik@gmail.com>
2019-06-20 19:25:51 +0530

    first commit

---
+++ lecture01_git_intro/file02.js
@@ -0,0 +1,9 @@
+function greet(name)
+{
+  console.log("knock knock knock" + name) 
+  console.log("knock knock knock" + name)   
+  console.log("knock knock knock" + name)    
+}
+greet("anirudh")
+console.log("use 'git status' to check status")
+console.log("use 'git log' to check history")
